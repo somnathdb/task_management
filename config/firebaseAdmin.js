f95927939eb232e294d4136faf9cf00e3e740876
somnathdb <somnathbhagwat0@gmail.com>
2026-02-07 10:27:33 +0530

    push new code

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,8 +1,25 @@
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
+
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
